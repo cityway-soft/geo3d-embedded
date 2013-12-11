13fb8166f9a82be8770e3b5743523214129a4579
Florent LABOUROT <flabourot@cityway.fr>
2013-12-11 12:01:40 +0100

    recup comptage

--- org.avm.business.comptage/src/org/avm/business/comptage/ComptageImpl.java
+++ org.avm.business.comptage/src/org/avm/business/comptage/ComptageImpl.java
@@ -3,7 +3,7 @@ package org.avm.business.comptage;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.Priority;
+import org.avm.business.comptage.bundle.Activator;
 import org.avm.business.core.AbstractAvmModelListener;
 import org.avm.business.core.Avm;
 import org.avm.business.core.AvmInjector;
@@ -15,6 +15,7 @@ import org.avm.elementary.common.Config;
 import org.avm.elementary.common.ConfigurableService;
 import org.avm.elementary.common.ConsumerService;
 import org.avm.elementary.common.ManageableService;
+import org.avm.elementary.geofencing.Balise;
 import org.avm.elementary.jdb.JDB;
 import org.avm.elementary.jdb.JDBInjector;
 import org.osgi.util.measurement.State;
@@ -23,30 +24,34 @@ public class ComptageImpl implements Comptage, ManageableService,
 		ConfigurableService, ComptageInjector, AvmInjector, ConsumerService,
 		JDBInjector {
 
-	private Logger _log = Logger.getInstance(this.getClass());
-	private ComptageConfig _config;
+	private Logger _log = Activator.getDefault().getLogger();
+	// private ComptageConfig _config;
 	private org.avm.device.comptage.Comptage _comptage;
 	private ModelListener _listener;
 	private boolean _initialized;
 	private Avm _avm;
 	private JDB _jdb;
-
-	public ComptageImpl() {
-		_log.setPriority(Priority.DEBUG);
-	}
+	int _last;
+	boolean _isStopDone = false;
+	boolean _isEndComptageDone = false;
+	private boolean _onecourse;
+	private Point _firstPoint = null;
+	private String _nomCourse = "";
+	private String _nomLigne = "";
+	private int _codeGirouette = 0;
+	private String _lastName;
 
 	public void configure(Config config) {
-		_config = (ComptageConfig) config;
+		// _config = (ComptageConfig) config;
 	}
 
 	public void setComptage(org.avm.device.comptage.Comptage comptage) {
-		_log.debug("set comptage " + comptage);
+		_log.info("set comptage " + comptage);
 		_comptage = comptage;
 		initialize();
 	}
 
 	public void unsetComptage(org.avm.device.comptage.Comptage comptage) {
-		_log.debug("unset comptage " + comptage);
 		_comptage = null;
 		_initialized = false;
 	}
@@ -65,16 +70,30 @@ public class ComptageImpl implements Comptage, ManageableService,
 
 	}
 
+	private void endComptage() {
+		Properties pp = getStatus();
+		StringBuffer log = new StringBuffer();
+		log.append("ENDCOMPTAGE;");
+		log.append(pp
+				.getProperty(org.avm.device.comptage.Comptage.NOMBRE_MONTEES));
+		log.append(';');
+		log.append(pp
+				.getProperty(org.avm.device.comptage.Comptage.NOMBRE_DESCENTES));
+		_jdb.journalize("comptage", log.toString());
+		// _comptage.miseAZero();
+	}
+
 	public void stop() {
 
 	}
 
 	private void initialize() {
 		if (!_initialized) {
-			_log.debug("Initialize / Comptage service=" + _comptage);
+			_log.info("initialize comptage");
 			if (_avm != null && _comptage != null) {
 				_listener = new ModelListener(_avm);
 				_listener.notify(_avm.getModel().getState());
+				// _comptage.miseAZero();
 				_initialized = true;
 			}
 		}
@@ -82,82 +101,177 @@ public class ComptageImpl implements Comptage, ManageableService,
 
 	public void notify(Object o) {
 		if (o instanceof State) {
+			// State state = (State) o;
 			if (_listener != null) {
 				_listener.notify(o);
 			}
+		} else if (o instanceof Balise) {
+			if (_firstPoint != null) {
+				Balise b = (Balise) o;
+				if (b.getId() == _firstPoint.getId()) {
+					initComptage();
+					_firstPoint = null;
+				}
+			}
+
+		}
+	}
+
+	private void initComptage() {
+		Properties pp = getStatus();
+		if (pp != null) {
+
+			StringBuffer log = new StringBuffer();
+			log.append("INITPASSAGERS;");
+			log.append(_nomLigne);
+			log.append(';');
+			log.append(_nomCourse);
+			log.append(';');
+			log.append(_codeGirouette);
+			log.append(';');
+			log.append(pp
+					.getProperty(org.avm.device.comptage.Comptage.NOMBRE_MONTEES));
+			log.append(';');
+			log.append(pp
+					.getProperty(org.avm.device.comptage.Comptage.NOMBRE_DESCENTES));
+			_log.info("onStateAttenteSaisieCourse: journalize");
+			_jdb.journalize("comptage", log.toString());
+			_onecourse = true;
+			miseAZero();
 		}
 	}
 
 	class ModelListener extends AbstractAvmModelListener {
 
 		private Avm _avm;
-		Point _lastPoint;
-
-		// private String _lastName;
-		//
-		// int _last;
 
 		public ModelListener(Avm avm) {
 			super(avm);
 			_avm = avm;
+			_onecourse = false;
+		}
+
+		protected void onStateAttenteSaisieCourse(AvmModel model) {
+			_log.info("onStateAttenteSaisieCourse");
+			if (_onecourse) {
+				if (!_isStopDone) {
+					journalizeArret();
+				}
+				endComptage();
+			}
 		}
 
 		protected void onStateAttenteDepart(AvmModel model) {
-			_log.debug("Comptage service=" + _comptage);
-			_comptage.miseAZero();
-			Properties pp = _comptage.status();
+			_log.info("onStateAttenteDepart");
 
 			Course course = model.getCourse();
-			String nomLigne = "";
-			String nomCourse = "";
+			_nomLigne = "";
+			_nomCourse = "";
+			_codeGirouette = 0;
 			if (course != null) {
-				nomCourse = course.getNom();
-				nomLigne = course.getLigneNom();
+				_nomCourse = course.getNom();
+				_nomLigne = course.getLigneNom();
 			}
-			StringBuffer log = new StringBuffer();
-			log.append("INITPASSAGERS;");
-			log.append(nomLigne);
-			log.append(';');
-			log.append(nomCourse);
-			log.append(';');
-			log.append(model.getCodeGirouette());
-			log.append(';');
-			log.append(pp
-					.getProperty(org.avm.device.comptage.Comptage.NOMBRE_DESCENTES));
-			log.append(';');
-			log.append(pp
-					.getProperty(org.avm.device.comptage.Comptage.NOMBRE_MONTEES));
-			_jdb.journalize("comptage", log.toString());
+			_codeGirouette = model.getCodeGirouette();
+
+			Point last = model.getDernierPoint();
+			if (last != null) // on est dans le point de départ
+			{
+				initComptage();
+				_lastName = last.getNom();
+				_firstPoint = null;
+			} else {
+				_firstPoint = model.getProchainPoint();
+				_lastName = _firstPoint.getNom();
+			}
 		}
 
 		protected void onStateEnCourseArretSurItineraire(AvmModel model) {
-			_lastPoint = _avm.getModel().getDernierPoint();
-			// _last = _avm.getModel().getDernierPoint().getId();
-			// _lastName = _avm.getModel().getDernierPoint().getNom();
+			Point point = _avm.getModel().getDernierPoint();
+			if (point != null) {
+				_last = point.getId();
+				_lastName = point.getNom();
+				_isStopDone = false;
+			}
 		}
 
 		protected void onStateEnCourseInterarretSurItineraire(AvmModel model) {
-			if (_lastPoint == null) {
-				_log.warn("lastPoint is null ? => à analyser ! (model=" + model
-						+ ")");
-
-				_lastPoint = model.getDernierPoint();
-			}
-			int last = _avm.getModel().getDernierPoint().getId();
-			String lastName = _avm.getModel().getDernierPoint().getNom();
-			_log.debug("Comptage service=" + _comptage);
-			Properties pp = _comptage.status();
-			_jdb.journalize(
-					"comptage",
-					"PASSAGERS;"
-							+ last
-							+ ";"
-							+ lastName
-							+ ";"
-							+ pp.getProperty(org.avm.device.comptage.Comptage.NOMBRE_MONTEES)
-							+ ";"
-							+ pp.getProperty(org.avm.device.comptage.Comptage.NOMBRE_DESCENTES));
+			_log.info("onStateEnCourseInterarretSurItineraire " + _jdb);
+			journalizeArret();
 		}
+
+		private void journalizeArret() {
+			Properties pp = getStatus();
+			if (pp != null) {
+				_jdb.journalize(
+						"comptage",
+						"PASSAGERS;"
+								+ _last
+								+ ";"
+								+ _lastName
+								+ ";"
+								+ pp.getProperty(org.avm.device.comptage.Comptage.NOMBRE_MONTEES)
+								+ ";"
+								+ pp.getProperty(org.avm.device.comptage.Comptage.NOMBRE_DESCENTES)
+								+ ";"
+								+ pp.getProperty(org.avm.device.comptage.Comptage.STATUS));
+				miseAZero();
+				_isStopDone = true;
+			}
+		}
+	}
+
+	private void sleepms(int time) {
+		try {
+			Object o = new Object();
+			synchronized (o) {
+				o.wait(time);
+			}
+		} catch (InterruptedException e) {
+
+		}
+	}
+
+	private boolean miseAZero() {
+		boolean ret = _comptage.miseAZero();
+		if (ret == false) {
+			int i = 0;
+			for (i = 0; i < 5; ++i) {
+				ret = _comptage.miseAZero();
+				if (ret) {
+					break;
+				}
+				sleepms(2000);
+			}
+			if (!ret) {
+				_jdb.journalize("comptage", "ERROR;reset impossible");
+			} else if (i != 0) {
+				_jdb.journalize("comptage", "ERROR;reset after " + i + " retry");
+			}
+		}
+		return ret;
+	}
+
+	private Properties getStatus() {
+		Properties pp = null;
+		pp = _comptage.status();
+		if (pp == null) {
+			int i = 0;
+			for (i = 0; i < 5; ++i) {
+				sleepms(2000);
+				pp = _comptage.status();
+				if (pp != null) {
+					break;
+				}
+			}
+			if (pp == null) {
+				_jdb.journalize("comptage", "ERROR;no value for status");
+			} else {
+				_jdb.journalize("comptage", "ERROR;value for status after " + i
+						+ " retry");
+			}
+		}
+		return pp;
 	}
 
 	public void setJdb(JDB jdb) {
