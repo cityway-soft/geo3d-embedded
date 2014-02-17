18ec0c5f390c22cc061a15e004b8410178978614
Didier LALLEMAND <dlallemand@cityway.fr>
2014-02-17 16:04:16 +0100

    ajustements dans pom.xml pour les T.U

--- org.avm.device.generic.afficheur.hanover/test/org/avm/device/generic/afficheur/hanover/TestGTMH_1.java
+++ org.avm.device.generic.afficheur.hanover/test/org/avm/device/generic/afficheur/hanover/TestGTMH_1.java
@@ -18,14 +18,19 @@ public class TestGTMH_1 extends TestCase {
 
 	private String generateStatus() {
 		AfficheurProtocol protocol2 = AfficheurProtocolFactory.create("GTMH_1");
-		return AfficheurProtocol.toHexaAscii(protocol2.generateStatus());
+		byte[] status = protocol2.generateStatus();
+		String hex = null;
+		if (status != null){
+			hex = AfficheurProtocol.toHexaAscii(status);
+		}
+		return hex;
 	}
 
 	public void testCode3() {
 		String code = "3";
 		String trame = generate(code);
 
-		String expected = "0230387A30303033373103";
+		String expected = "02303033033641";
 		debug("code=" + code + "   =>" + trame);
 		assertEquals(trame.length(), expected.length());
 		assertEquals(expected, trame);
@@ -34,7 +39,7 @@ public class TestGTMH_1 extends TestCase {
 	public void testCode03() {
 		String code = "03";
 		String trame = generate(code);
-		String expected = "0230387A30303033373103";
+		String expected = "0230303033033341";
 		debug("code=" + code + "  =>" + trame);
 		assertEquals(trame.length(), expected.length());
 		assertEquals(expected, trame);
@@ -43,7 +48,7 @@ public class TestGTMH_1 extends TestCase {
 	public void testCode27() {
 		String code = "27";
 		String trame = generate(code);
-		String expected = "0230387A30303237373703";
+		String expected = "0230303237033334";
 		debug("code=" + code + "  =>" + trame);
 		assertEquals(trame.length(), expected.length());
 		assertEquals(expected, trame);
@@ -52,7 +57,7 @@ public class TestGTMH_1 extends TestCase {
 	public void testCode123() {
 		String code = "123";
 		String trame = generate(code);
-		String expected = "0230387A30313233373203";
+		String expected = "023030313233033037";
 		debug("code=" + code + " =>" + trame);
 		assertEquals(trame.length(), expected.length());
 		assertEquals(expected, trame);
@@ -61,7 +66,7 @@ public class TestGTMH_1 extends TestCase {
 	public void testCode999() {
 		String code = "999";
 		String trame = generate(code);
-		String expected = "0230387A30393939373B03";
+		String expected = "023030393939034632";
 		debug("code=" + code + "=>" + trame);
 		assertEquals(trame.length(), expected.length());
 		assertEquals(expected, trame);
@@ -70,18 +75,11 @@ public class TestGTMH_1 extends TestCase {
 	public void testCode9999() {
 		String code = "9999";
 		String trame = generate(code);
-		String expected = "0230387A39393939373203";
+		String expected = "02303039393939034239";
 		debug("code=" + code + "=>" + trame);
 		assertEquals(trame.length(), expected.length());
 		assertEquals(expected, trame);
 	}
 
-	public void testGenerateStatus() {
-		String trame = generateStatus();
-		String expected = "0230387A39393939373203";
-		debug("status request=" + trame);
-		assertEquals(trame.length(), expected.length());
-		assertEquals(expected, trame);
-	}
 
 }
---
+++ org.avm.device.generic.comptage.elinap/test_DISABLED/org/avm/device/generic/comptage/elinap/TestComptage.java
@@ -0,0 +1,16 @@
+package org.avm.device.generic.comptage.elinap;
+
+import junit.framework.TestCase;
+
+import org.avm.device.generic.comptage.elinap.ProtocolHelper;
+
+public class TestComptage extends TestCase {
+
+	public void debug(String string) {
+		System.out.println(string);
+	}
+
+	public void testXXXX() {
+		//TODO
+	}
+}
