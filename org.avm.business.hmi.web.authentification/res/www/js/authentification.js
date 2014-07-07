4039362a5a1b50dc712c02bcc84ff26da5f2f8f7
Florent LABOUROT <flabourot@cityway.fr>
2014-07-07 15:37:43 +0200

    move

--- org.avm.business.hmi.web.authentification/res/www/js/authentification.js
+++
@@ -1,7 +0,0 @@
-
-/**
- * récupère les champs et poste la requete
- */
-function dologin (){
-	$.post("/authentification", { action: "login", login: $("#login").val(), password: $("#password").val()} );
-}
