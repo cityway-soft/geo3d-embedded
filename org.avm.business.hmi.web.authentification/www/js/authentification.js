ff83df48a4c42789ccc8496ae100fb5b8266616a
Florent LABOUROT <flabourot@cityway.fr>
2014-07-09 14:23:00 +0200

    clavier js à la place du champs classique

--- org.avm.business.hmi.web.authentification/www/js/authentification.js
+++ org.avm.business.hmi.web.authentification/www/js/authentification.js
@@ -1,7 +1,27 @@
-
+var authState = 0;
+var authMat = "";
 /**
  * récupère les champs et poste la requete
  */
-function dologin (){
-	$.post("/authentification", { action: "login", login: $("#login").val(), password: $("#password").val()} );
+function dologin (login, pass){
+	$.post("/authentification", { action: "login", login: login, password: pass} );
+}
+
+function initAuth(){
+	NumKeyboard.init("authkb", "authText", onValidate);
+}
+
+function onValidate(value){
+	if (authState == 0){
+		authState = 1;
+		authMat = value;
+		$("#authtitle").text("Code");
+		$("#authText").val("");
+	}else if (authState == 1){
+		authState = 0;
+		dologin(authMat, value);
+		authMat = "";
+		$("#authtitle").text("Matricule");
+		$("#authText").val("");
+	}
 }
