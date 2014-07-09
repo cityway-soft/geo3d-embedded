859b8aea16c3ce07c393c528d9ef3d1148eefe44
Florent LABOUROT <flabourot@cityway.fr>
2014-07-09 14:32:03 +0200

    creation

---
+++ org.avm.business.hmi.web.desktop/src/org/avm/business/hmi/web/desktop/DesktopInfosUser.java
@@ -0,0 +1,10 @@
+package org.avm.business.hmi.web.desktop;
+
+public interface DesktopInfosUser {
+
+	public String getJS();
+	public String getName();
+	public String getDivContent();
+	public int getOrder();
+
+}
