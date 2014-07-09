b83cb2e52b681ed9feb28f7bca3cf02863799781
Florent LABOUROT <flabourot@cityway.fr>
2014-07-09 14:28:24 +0200

    mutualisation du code avec utils

--- org.avm.business.hmi.web.avm/src/org/avm/business/hmi/web/avm/servlet/AvmServlet.java
+++ org.avm.business.hmi.web.avm/src/org/avm/business/hmi/web/avm/servlet/AvmServlet.java
@@ -10,15 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 import org.avm.business.core.Avm;
 import org.avm.business.core.AvmModel;
 import org.avm.business.core.event.Course;
+import org.avm.business.hmi.web.utils.HmiServlet;
+import org.avm.business.hmi.web.utils.LongPollingWaiter;
+import org.avm.business.hmi.web.utils.ResponseWaiter;
 import org.avm.elementary.common.ConsumerService;
 import org.avm.elementary.useradmin.UserSessionService;
 import org.osgi.util.measurement.State;
 import org.stringtree.json.JSONWriter;
 
-public class AvmServlet extends HttpServlet implements ConsumerService {
+public class AvmServlet extends HmiServlet implements ConsumerService {
 
 	private Avm avm;
-	private Object lock = new Object();
+	private LongPollingWaiter responseWaiter = new LongPollingWaiter();
 
 	public void setAvm(Avm avm) {
 		this.avm = avm;
@@ -26,11 +29,10 @@ public class AvmServlet extends HttpServlet implements ConsumerService {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		System.out.println(req.getPathInfo());
 		if (avm != null) {
 			String path = req.getPathInfo();
 			if (path.equals("/waitstate")) {
-				wait4State(resp);
+				wait4State(req, resp);
 			} else if (path.equals("/listcourses")) {
 				listCourses(resp);
 			}
@@ -49,41 +51,13 @@ public class AvmServlet extends HttpServlet implements ConsumerService {
 		sendJSON(resp, simpleCourses);
 	}
 
-	private void sendJSON(HttpServletResponse resp, Object obj)
+	private void wait4State(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		JSONWriter writer = new JSONWriter();
-		String text = writer.write(obj);
-		resp.setContentType("application/json");
-		resp.getOutputStream().write(text.getBytes());
-	}
-
-	private void wait4State(HttpServletResponse resp) throws IOException {
-		try {
-			synchronized (lock) {
-				lock.wait(10000);
-			}
-		} catch (InterruptedException e) {
-
+		if (!isNoWait(req)) {
+			responseWaiter.waitToDoResponse(req);
 		}
 		AvmModel model = avm.getModel();
 		sendJSON(resp, model);
-	}
-
-	private void wait4JSON() {
-		try {
-			synchronized (lock) {
-				lock.wait(10000);
-			}
-		} catch (InterruptedException e) {
-
-		}
-
-	}
-
-	private void unblockJSON() {
-		synchronized (lock) {
-			lock.notify();
-		}
 	}
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
@@ -96,43 +70,32 @@ public class AvmServlet extends HttpServlet implements ConsumerService {
 				if (iidu != -1) {
 					avm.priseCourse(iidu);
 				}
-			}else if (path.equals("/depart")) {
+			} else if (path.equals("/priseservice")) {
+				String idu = req.getParameter("idu");
+				int iidu = getInt(idu);
+				if (iidu != -1) {
+					avm.priseService(iidu);
+				}
+			} else if (path.equals("/depart")) {
 				avm.depart();
-			}else if (path.equals("/finservice")){
+			} else if (path.equals("/finservice")) {
 				avm.finService();
-				
-			}else if (path.equals("/fincourse")){
+
+			} else if (path.equals("/fincourse")) {
 				avm.finCourse();
-				
-			}else if (path.equals("/deviation")){
+
+			} else if (path.equals("/deviation")) {
 				avm.sortieItineraire();
-			}else if (path.equals("/priseposte")){
+			} else if (path.equals("/priseposte")) {
 				avm.prisePoste(0, 0);
 			}
 		}
 	}
 
-	private int getInt(String val) {
-		int ret = -1;
-		try {
-			ret = Integer.parseInt(val);
-		} catch (NumberFormatException nfe) {
-
-		}
-		return ret;
-	}
-
 	public void notify(Object o) {
 		if (o instanceof State) {
-			State state = (State) o;
-			unblockJSON();
-			if (state.getName().equals(UserSessionService.class.getName())) {
-				// notifyUserSessionService(state);
-			}
-			/*
-			 * else if (_session != null && _session.getState().getValue() ==
-			 * UserSessionService.AUTHENTICATED) { //notifyAvm(state); }
-			 */
+			System.out.println("in notify ");
+			responseWaiter.unlockRequest();
 		}
 	}
 }
