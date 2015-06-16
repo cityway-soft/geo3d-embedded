0653914b3061dc3873226141bb3b60585b38cdec
Didier LALLEMAND <dlallemand@cityway.fr>
2015-06-16 16:14:09 +0200

    fixed : bundles gps, media.* will be stopped on APC off (and restarted on APC on)

--- org.avm.device.wanesy.standby/src/org/avm/device/wanesy/standby/DeviceWanesyStandby.java
+++ org.avm.device.wanesy.standby/src/org/avm/device/wanesy/standby/DeviceWanesyStandby.java
@@ -1,36 +1,20 @@
 package org.avm.device.wanesy.standby;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.avm.device.knet.model.KmsMarshaller;
 import org.avm.device.knet.model.KmsStandbyTrig;
 import org.avm.device.knet.model.KmsSystem;
-import org.avm.device.wanesy.DeviceWanesy;
 import org.avm.device.wanesy.DeviceWanesyListener;
 import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleException;
-import org.osgi.service.startlevel.StartLevel;
 
 public class DeviceWanesyStandby implements DeviceWanesyListener {
 
 	private Logger log = Logger.getInstance(this.getClass());
 
-	private DeviceWanesy deviceWanesy = null;
-
-	private StartLevel sl = null;
-
-	private static final int EXIT_CODE = 2;
-
-	private static final int START_LEVEL = 1;
-
-	private Bundle gps = null;
-
-	public Bundle getGps() {
-		return gps;
-	}
-
-	public void setGps(Bundle gps) {
-		this.gps = gps;
-	}
+	private List bundlesToStop = null;
 
 	public DeviceWanesyStandby() {
 	}
@@ -43,51 +27,49 @@ public class DeviceWanesyStandby implements DeviceWanesyListener {
 		if (kmsMarshaller.getRole().equals("standby")) {
 			KmsStandbyTrig standbyTrig = (KmsStandbyTrig) kmsMarshaller;
 			log.info("Standby called with delay :  " + standbyTrig.getDelay());
-			stopGPS();
+			stopBundles();
 		} else if (kmsMarshaller.getRole().equals("system")) {
 			KmsSystem system = (KmsSystem) kmsMarshaller;
 			log.info("System with cause :  " + system.getAct());
 			if ("wakeup".equals(system.getAct())) {
-				startGPS();
+				startBundles();
 			}
 		}
 	}
 
-	private void startGPS() {
-		if (gps != null) {
-			try {
-				gps.start();
-			} catch (BundleException e) {
-				log.error("cannot start gps", e);
+	private void stopBundles() {
+
+		if (bundlesToStop != null) {
+			Iterator iter = bundlesToStop.iterator();
+			while(iter.hasNext()){
+				try {
+					Bundle bundle = (Bundle)iter.next();
+					log.info("Bundle "+ bundle.getSymbolicName() + " stopping...");
+					bundle.stop();
+					log.info("Bundle "+ bundle.getSymbolicName() + " stopped.");
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
 
-	private void stopGPS() {
-		try {
-			gps.stop();
-		} catch (BundleException e) {
-			log.error("cannot stop gps", e);
-		}
-	}
+	private void startBundles() {
+		if (bundlesToStop != null) {
+			Iterator iter = bundlesToStop.iterator();
+			while(iter.hasNext()){
+				try {
+					Bundle bundle = (Bundle)iter.next();
+					log.info("Bundle "+ bundle.getSymbolicName() + " starting...");
+					bundle.start();
+					log.info("Bundle "+ bundle.getSymbolicName() + " started.");
 
-	private void stopApplication() {
-
-		sl.setStartLevel(START_LEVEL);
-		long now = System.currentTimeMillis();
-		while (sl.getStartLevel() != START_LEVEL) {
-			sleep(1000);
-			if (System.currentTimeMillis() - now > 30 * 1000) {
-				log.debug("shutdown timeout ");
-				break;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
-
-		System.exit(EXIT_CODE);
-	}
-
-	public void setWanesy(DeviceWanesy deviceWanesy) {
-		this.deviceWanesy = deviceWanesy;
 	}
 
 	public void start() {
@@ -98,15 +80,15 @@ public class DeviceWanesyStandby implements DeviceWanesyListener {
 
 	}
 
-	public void setStartLevelService(StartLevel sl) {
-		this.sl = sl;
-	}
-
 	protected void sleep(int millis) {
 		try {
 			Thread.sleep(millis);
 		} catch (InterruptedException e) {
 		}
+	}
+
+	public void setBundlesToStop(List bundlesToStop2) {
+		this.bundlesToStop = bundlesToStop2;
 	}
 
 }
--- org.avm.device.wanesy.standby/src/org/avm/device/wanesy/standby/bundle/Activator.java
+++ org.avm.device.wanesy.standby/src/org/avm/device/wanesy/standby/bundle/Activator.java
@@ -1,5 +1,8 @@
 package org.avm.device.wanesy.standby.bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.avm.device.wanesy.DeviceWanesy;
 import org.avm.device.wanesy.standby.DeviceWanesyStandby;
@@ -7,57 +10,53 @@ import org.avm.elementary.common.AbstractActivator;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
-import org.osgi.service.startlevel.StartLevel;
 
-public class Activator extends AbstractActivator  {
+public class Activator extends AbstractActivator {
 
 	private DeviceWanesyStandby peer;
 
-	
 	private DeviceWanesy deviceWanesy;
 
+	private static final String[] BUNDLES_TO_STOP = {
+			"org.avm.device.generic.gps", "org.avm.elementary.media." };
+
 	private Logger log = Logger.getInstance(this.getClass());
-	
+
 	public Activator() {
 		peer = new DeviceWanesyStandby();
 	}
 
-	
-
-
 	protected void start(ComponentContext context) {
 		log.info("on Start");
 		deviceWanesy = (DeviceWanesy) context.locateService("wanesy");
+		
 		deviceWanesy.addDeviceListener(peer);
-		retrieveStartLevelService(context);
 		retrieveGPS(context.getBundleContext());
-		peer.setWanesy (deviceWanesy);
-		peer.start ();
+		peer.start();
 	}
 
 	private void retrieveGPS(BundleContext bundleContext) {
 		Bundle[] list = bundleContext.getBundles();
-		for (int i = 0; i < list.length; ++i){
-			if ("org.avm.device.generic.gps".equals(list[i].getSymbolicName())){
-				peer.setGps(list[i]);
-				break;
+		List bundlesToStop = new ArrayList();
+		for (int i = 0; i < list.length; ++i) {
+			for (int j = 0; j < BUNDLES_TO_STOP.length; j++) {
+				if (list[i].getSymbolicName().indexOf(BUNDLES_TO_STOP[j]) != -1) {
+					bundlesToStop.add(list[i]);
+
+					if (bundlesToStop.size() == BUNDLES_TO_STOP.length) {
+						break;
+					}
+				}
 			}
 		}
+		peer.setBundlesToStop(bundlesToStop);
 	}
-
-
-
 
 	protected void stop(ComponentContext context) {
-		peer.stop ();
+		peer.stop();
 		log.info("on stop");
 		deviceWanesy.removeDeviceListener(peer);
-		
-	}
-	
-	private void retrieveStartLevelService (ComponentContext cc){
-		StartLevel sl = (StartLevel)cc.locateService(StartLevel.class.getName());
-		peer.setStartLevelService(sl);
+
 	}
 
 }
